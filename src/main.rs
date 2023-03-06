0600b9bde3b4c6ad005f928d746b81385abdc47d
carghai <carghai74@gmail.com>
2023-03-06 08:19:57 -0500

    works on linux

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use pyo3::prelude::*;
 
 fn main() {
+    for _ in 0..200 {
+        println!("python says: {}", python());
+    }
+}
+
+fn python() -> String {
     let py_app = include_str!("long.py");
     let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
-        let app: Py<PyAny> = PyModule::from_code(py, py_app, "long.py", "").unwrap()
+        let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")
+            .unwrap()
             .getattr("test")?
             .into();
         app.call0(py)
     });
-    println!("py: {}", from_python.unwrap());
+    return from_python.unwrap().to_string();
 }
