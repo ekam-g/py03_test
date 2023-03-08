0cd72a94e4eed4f762ce26143c7d4027d31b4e6a
carghai <carghai74@gmail.com>
2023-03-08 09:06:59 -0500

    python worjs

--- src/long.py
+++ src/long.py
@@ -1,8 +1,8 @@
 
 def test():
-    cool = "works"
-    nice = 0
-    for x in range(1_000_000):
-        nice = 1000 / 10003 + 2000 - 2942345 + 12423 * 2353 /24343523 + 235235/32432+ 45643 *32541346 /35134613
-    print(cool)
-    return "funny"
+    data = requests.get('https://api.github.com').json()
+    print(data)
+    try: 
+        return data["following_url"]
+    except:
+        return "error bro"
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
 fn python() -> String {
     let py_app = include_str!("long.py");
     let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+        let _ = py.import("requests").unwrap();
         let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")
             .unwrap()
             .getattr("test")
